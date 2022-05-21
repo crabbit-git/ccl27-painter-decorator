991960d08ceb3a379454e641d6577d85eeaeefed
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-05-21 22:36:40 +0100

    Decorator can stock up

--- models/decorator.js
+++ models/decorator.js
@@ -1,4 +1,14 @@
 const Decorator = function() {
     this.stock = [];
 }
+Decorator.prototype.addPaint = function(paint) {
+    this.stock.push(paint);
+}
+Decorator.prototype.totalPaintVolume = function() {
+    let totalPaintVolume = 0;
+    for (let paint of this.stock) {
+        totalPaintVolume += paint.volume;
+    }
+    return totalPaintVolume;
+}
 module.exports = Decorator;
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -4,10 +4,12 @@ const Paint = require('../models/paint');
 
 describe('Decorator', function() {
 
-    let paint;
+    let crownMattEmulsion;
+    let duluxEgyptianCotton;
     let decorator;
 
     beforeEach(function() {
+        crownMattEmulsion = new Paint(10);
         duluxEgyptianCotton = new Paint(5);
         decorator = new Decorator();
     });
@@ -16,9 +18,16 @@ describe('Decorator', function() {
         assert.strictEqual(decorator.stock.length, 0);
     });
 
-    it('should be able to add a can of paint to stock');
+    it('should be able to add a can of paint to stock', function() {
+        decorator.addPaint(duluxEgyptianCotton);
+        assert.deepStrictEqual(decorator.stock, [duluxEgyptianCotton]);
+    });
 
-    it('should be able to calculate total litres of paint in stock');
+    it('should be able to calculate total litres of paint in stock', function() {
+        decorator.addPaint(crownMattEmulsion);
+        decorator.addPaint(duluxEgyptianCotton);
+        assert.strictEqual(decorator.totalPaintVolume(), 15);
+    });
 
     it('should be able to ascertain whether there is enough paint in stock to paint a given room');
 
